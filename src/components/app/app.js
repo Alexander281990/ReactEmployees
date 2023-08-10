fe4dea3329e5b349e141d78cafa222a19d8f0243
Alexander <alexivanov901128@gmail.com>
2023-08-11 01:12:07 +0300

    continuation of the project

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,29 +6,96 @@ import EmployersList from '../employees-list/emploees-list';
 import EmployersAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
+import { Component } from 'react';
 
-function App() {
 
-    const data = [
-        {name: "Саша", salary: 800, increase: false, id: 1},
-        {name: "Маша", salary: 200, increase: true, id: 2},
-        {name: "Паша", salary: 1000, increase: false, id: 3},
-        {name: "Вася", salary: 1000, increase: false, id: 4},
-        {name: "Миша", salary: 500, increase: false, id: 5},
-    ];
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: "Саша", salary: 800, increase: false, id: 1},
+                {name: "Маша", salary: 200, increase: true, id: 2},
+                {name: "Паша", salary: 1000, increase: false, id: 3},
+                {name: "Вася", salary: 1000, increase: false, id: 4},
+                {name: "Миша", salary: 500, increase: false, id: 5},
+                {name: "Коля", salary: 650, increase: false, id: 6},
+            ],
+        }
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
+    // function WhoAmi({name, surname, link}) {
+    //     return (
+    //         <div>
+    //             <h1>My name is {name()}, surname - {surname}</h1>
+    //             <a href={link}>My profile</a>
+    //         </div>
+    //     )
+    // }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    // class WhoAmi extends Component {
+    //     constructor(props) {
+    //         super(props);
+    //         this.state = {
+    //             yerars: 27,
+    //             text: "+++",
+    //             position: "",
+    //         }
+    //     }
+    //     nextYear = () => {
+    //         this.setState(state => ({
+    //             yerars: state.yerars + 1,
+    //             text: "++",
+    //         }))
+    //     }
+    //     commitInputChanges = (e) => {
+    //         this.setState({
+    //             position: e.target.value,
+    //         })
+    //     }
+    //     render() {
+    //         const {name, surname, link} = this.props;
+    //         const {position, yerars} = this.state;
+    //         return (
+    //             <div>
+    //                 <button onClick={this.nextYear}>{this.state.text}</button>
+    //                 <h1>My name is {name}, surname - {surname}, age - {yerars}, position - {position}</h1>
+    //                 <a href={link}>My profile</a>
+    //                 <form>
+    //                     <span>Введите должность</span>
+    //                     <input type="text" onChange={this.commitInputChanges}/>
+    //                 </form>
+    //             </div>
+    //         )
+    //     }
+    // }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id != id),
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                {/* <WhoAmi name='John' surname="Smith" link="facebook.com"></WhoAmi>
+                <WhoAmi name='John' surname="Shepard" link="vk.com"></WhoAmi> */}
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployersList 
+                data={this.state.data}
+                onDelete={this.deleteItem}/>
+                <EmployersAddForm/>
             </div>
-            <EmployersList data={data}/>
-            <EmployersAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/employees-list/emploees-list.js
+++ src/components/employees-list/emploees-list.js
@@ -2,12 +2,15 @@
 import EmployersListItem from '../employees-list-item/employees-list-item';
 import './employers-list.css';
 
-const EmployersList = ({data}) => {
+const EmployersList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
         const {id, ...ietmProps} = item;
         return (
-            <EmployersListItem key={id} {...ietmProps}/>
+            <EmployersListItem 
+                key={id} 
+                {...ietmProps}
+                onDelete ={() => onDelete(id) }/>
             // <EmployersListItem name={item.name} salary={item.salary} />
         )
     })
